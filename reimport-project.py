1cef1f98123c7ed8e51f0b4dcb7c3f27dcdb7764
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-07-26 17:50:18 +0800

    Update reimport-project.py

--- reimport-project.py
+++ reimport-project.py
@@ -21,44 +21,26 @@ pa_map = {}
 for i in pa.find():
     pa_map[str(i['_id'])] = i['title']['zh-TW']
 
-include_project = [ObjectId('5ce75c2d4d063e3d8279342f'),
-ObjectId('5ceb8377f974a93509bb457a'),ObjectId('5d2d459c968e42333ce6f015'),
-ObjectId('5d2ebb415778d9207444101d'),
-ObjectId('5d2ebb42d861e5e7833adefb'),
-ObjectId('5d6e1682d48bef0d6819790d'),
-ObjectId('5d6f2a6cd48bef40d1199705'),
-ObjectId('5d79f7cbce838b6f2edab41b'),
-ObjectId('5d96d825ce838b3a7de64931'),
-ObjectId('5d9d4c00d109e75584da37ab'),
-ObjectId('5da97133c641733c60b26808'),
-ObjectId('5da97799c64173dc91b2696e'),
-ObjectId('5de74aa011bd907ad8fff166'),
-ObjectId('5de74abc11bd904442fff1b4'),
-ObjectId('5ce75c2b4d063e53db7933f9'),
-ObjectId('5ceb7916f974a96b6ebb322b'),
-ObjectId('5ceb6101f974a9bd5fbb2a23'),
-ObjectId('5ceb73ccf974a9ddf4bb2fa8'),
-ObjectId('5ce7ed824d063e6da4794015'),
-ObjectId('5ceb7d46f974a946efbb3b84'),
-ObjectId('5ceb8081f974a94c1fbb4383'),
-ObjectId('5ceb8026f974a9371bbb419d'),
-ObjectId('5ceb804ff974a9a319bb4277'),
-ObjectId('5ceb7ec0f974a956f6bb3f39'),
-ObjectId('5ce75c2c4d063eac2679340b'),
-ObjectId('5ceb83dff974a96b6bbb4672'),
-ObjectId('5ceb4c95f974a93e4ebb22a0'),
-ObjectId('5ceb83b5f974a9c396bb45ed'),
-ObjectId('5ceb58fbf974a96a90bb26f6'),
-ObjectId('5ce75c2c4d063e010b79341d'),
-ObjectId('5ceb7e0df974a9ea6abb3da9'),
-ObjectId('5ceb49d3f974a9251dbb222d'),
-ObjectId('5ceb6902f974a9755fbb2d8b'),
-ObjectId('5ceb831ef974a98e39bb449e'),
-ObjectId('5ceb57e9f974a92b5bbb2567'),
-ObjectId('5ceb7c2ff974a98437bb3843'),
-ObjectId('5ceb83dcf974a9bcadbb465e'),
-ObjectId('5de74ace11bd90f94ffff2ed'),
-ObjectId('5de74afc11bd90f8fffff56e')]
+include_project = [ObjectId('5ce75c2d4d063e3d8279342f'), ObjectId('5ceb8377f974a93509bb457a'),
+                   ObjectId('5d2d459c968e42333ce6f015'), ObjectId('5d2ebb415778d9207444101d'),
+                   ObjectId('5d2ebb42d861e5e7833adefb'), ObjectId('5d6e1682d48bef0d6819790d'),
+                   ObjectId('5d6f2a6cd48bef40d1199705'), ObjectId('5d79f7cbce838b6f2edab41b'),
+                   ObjectId('5d96d825ce838b3a7de64931'), ObjectId('5d9d4c00d109e75584da37ab'),
+                   ObjectId('5da97133c641733c60b26808'), ObjectId('5da97799c64173dc91b2696e'),
+                   ObjectId('5de74aa011bd907ad8fff166'), ObjectId('5de74abc11bd904442fff1b4'),
+                   ObjectId('5ce75c2b4d063e53db7933f9'), ObjectId('5ceb7916f974a96b6ebb322b'),
+                   ObjectId('5ceb6101f974a9bd5fbb2a23'), ObjectId('5ceb73ccf974a9ddf4bb2fa8'),
+                   ObjectId('5ce7ed824d063e6da4794015'), ObjectId('5ceb7d46f974a946efbb3b84'), 
+                   ObjectId('5ceb8081f974a94c1fbb4383'), ObjectId('5ceb8026f974a9371bbb419d'), 
+                   ObjectId('5ceb804ff974a9a319bb4277'), ObjectId('5ceb7ec0f974a956f6bb3f39'), 
+                   ObjectId('5ce75c2c4d063eac2679340b'), 
+                   ObjectId('5ceb83dff974a96b6bbb4672'), ObjectId('5ceb4c95f974a93e4ebb22a0'), 
+                   ObjectId('5ceb83b5f974a9c396bb45ed'), ObjectId('5ceb58fbf974a96a90bb26f6'), 
+                   ObjectId('5ce75c2c4d063e010b79341d'), ObjectId('5ceb7e0df974a9ea6abb3da9'),
+                   ObjectId('5ceb49d3f974a9251dbb222d'), ObjectId('5ceb6902f974a9755fbb2d8b'),
+                   ObjectId('5ceb831ef974a98e39bb449e'), ObjectId('5ceb57e9f974a92b5bbb2567'),
+                   ObjectId('5ceb7c2ff974a98437bb3843'), ObjectId('5ceb83dcf974a9bcadbb465e'),
+                   ObjectId('5de74ace11bd90f94ffff2ed'), ObjectId('5de74afc11bd90f8fffff56e')]
 
 project_list = projects.find({"_id":{"$in": include_project}})
 
@@ -71,7 +53,6 @@ for i in project_list:
     areas = []
     for x in i['areas']:
         areas.append(pa_map[str(x)])
-    print ('areas', ','.join(areas))
     p = Project(
         name = i['title'],
         description = i.get('description', ''),
@@ -91,7 +72,6 @@ for i in project_list:
         interpretive_data_license = i.get('interpretiveDataLicense', ''),
         identification_information_license = i.get('identificationInformationLicense', ''),
         video_material_license = i.get('videoMaterialLicense', ''),
-
         mode = "official"
     )
     p.save()
@@ -151,9 +131,7 @@ for i in sa.find({"project":{"$in": include_project}}):
 c = db['CameraLocations']
 c_map_data = {}
 for i in c.find({"project":{"$in": include_project}}):
-    # project_id = pmap_data[str(i['project'])]
-    project_name = project_name_map[str(i['project'])]
-    project_id = Project.objects.filter(name=project_name).first().id
+    project_id = pmap_data[str(i['project'])]
     # map study area
     study_area_id = sa_map_data[str(i['studyArea'])]
     d = Deployment(
